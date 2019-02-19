8feb362740008c1e95a958e185f25e62e92b65ea
Onur Bagoren <onurbagoren3@gmail.com>
2019-02-18 19:59:58 -0500

    .gitignore

--- src/interprocess_communication/src/interprocess_publisher.cpp
+++ src/interprocess_communication/src/interprocess_publisher.cpp
@@ -3,8 +3,7 @@
 #include "interprocess_communication/interprocess_communication.h"
 #include "interprocess_communication/Update.h"
 using namespace std;
-int
-main( int argc, char* argv[] ){
+int main( int argc, char* argv[] ){
 	ros::init( argc, argv, "interprocess_communication_publisher" );
 	ros::NodeHandle node_handle;
 	ros::Publisher update_publisher = node_handle.advertise< interprocess_communication::Update >( "update", 1, true);
